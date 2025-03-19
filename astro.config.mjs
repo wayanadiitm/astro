fa0e45355b094a9490d8ff3f9eec729abeaa7743
wayanad house 2023 archives <150238196+wayanadiitm@users.noreply.github.com>
2025-03-19 23:40:13 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import icon from 'astro-icon'
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  site: 'https://accessible-astro.netlify.app',
+  site: 'https://wayanadiitm.github.io',
+  base: 'astro',
   integrations: [
     mdx(),
     icon(),
